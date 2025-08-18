eb3851e1b74fd19b0f5ec00c760bb525e2ae4c60
sandhya144 <186sndy@gmail.com>
2025-08-18 13:24:35 +0530

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default RootLayout;
---
+++ components/Authform.tsx
@@ -0,0 +1,9 @@
+import react from "react";  
+
+const Authform = ({ type }: { type: "sign-in" | "sign-up" }) => {
+  return (
+    <div></div>
+  );
+};
+
+export default Authform;
