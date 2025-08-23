215400ce39cb2607515270fb8bc248aeb1067d7a
sandhya144 <186sndy@gmail.com>
2025-08-23 19:12:58 +0530

    msg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import {ReactNode} from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div >
+    <div className="auth-layout">
      {children}
     </div>
   );
