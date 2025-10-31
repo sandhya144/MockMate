6db0d54fdf07939f4dc5dd3ed6a58f007c8f58b4
sandhya144 <186sndy@gmail.com>
2025-10-31 17:49:31 +0530

    msg

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,18 +1,24 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import admin from "firebase-admin";
 
 // Initialize Firebase Admin SDK
 function initFirebaseAdmin() {
   const apps = getApps();
 
   if (!apps.length) {
+
+   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+
     initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
+
+      // credential: cert({
+      //   projectId: process.env.FIREBASE_PROJECT_ID,
+      //   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      //   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      // }),
+      credential: cert(serviceAccount),
     });
   }
 
